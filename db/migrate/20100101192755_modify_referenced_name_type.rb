d93cdf1d19c0cfbe6799e53072adde0076162ccd
Nicolas Orcali <orcalinicolas@gmail.com>
2017-09-04 09:36:30 -0300

    up

--- db/migrate/20100101192755_modify_referenced_name_type.rb
+++
@@ -1,9 +0,0 @@
-class ModifyReferencedNameType < ActiveRecord::Migration
-  def self.up
-    change_column :wiki_references, :referenced_name, :string, :limit => 255
-  end
-
-  def self.down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
